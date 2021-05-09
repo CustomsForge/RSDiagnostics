b52e2a55fd35aba54180fc560c82e22b4a1a325e
ffio1 <ffio1@users.noreply.github.com>
2021-05-08 17:03:22 -0700

    Totally my code :P

--- Program.cs
+++ Program.cs
@@ -19,6 +19,8 @@ namespace RSDiagnostics
             Application.Run(new MainForm());
         }
 
+
+        // Give credit where credit is due: https://stackoverflow.com/a/6362414
         static Assembly ResolveEmbededAssembly(object sender, ResolveEventArgs args)
         {
             string dllName = args.Name.Contains(",") ? args.Name.Substring(0, args.Name.IndexOf(',')) : args.Name.Replace(".dll", "");
